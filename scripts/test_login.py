0f4950c59f3d75cf531450318e1d431c3bc3ba57
nana <806828403@qq.com>
2018-03-30 18:23:59 +0800

    提交

---
+++ scripts/test_login.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+class TestLogin:
+
+    def test_log1(self):
+        assert 1
+
+    def test_log2(self):
+        assert 1
+
+    def test_log3(self):
+        assert 1
+
+    def test_log4(self):
+        assert 1
